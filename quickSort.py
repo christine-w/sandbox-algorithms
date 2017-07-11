2e34bde3aaf80b4da968190ba96affc016b00b96
christine-w <christine.wong@gmail.com>
2017-07-11 09:25:12 -0700

    partition subroutine

--- quickSort.py
+++ quickSort.py
@@ -1,26 +1,23 @@
-# python2
 '''
 quickSort.py
-input: an array of integers
-output: array of integers sorted in ascending order
+input: an array of integers, leftmost index, rightmost index (i.e., len(array) = right - left + 1), (optional) running total of comparisons
+output: array is sorted in place, return value is the total number of comparisons made
 
 quick sort has an average running time of O(nlogn)
 '''
 import random
-import sys
 
-def quickSort(arr, left, right, numComparisons):
+def quickSort(arr, left, right, numComparisons=[0]):
     #print 'left: ' + str(left) + ' right: ' + str(right) + ' array: ' + str(arr)
     if left >= right:
         #print 'BASE CASE REACHED'
-        return
+        return numComparisons
 
     numComparisons[0] += right - left
-    #print(right - left)
-    #pivotIndex = random.randint(left, right)
-    #pivotIndex = left      #question1
-    pivotIndex = right     #question2
     '''
+    #pivotIndex = left
+    #pivotIndex = right
+    # median of three pivot choice
     medianIndex = (right - left)//2
     medianValue = sorted([arr[left], arr[left+medianIndex], arr[right]])[1]
     if arr[left] == medianValue:
@@ -29,7 +26,21 @@ def quickSort(arr, left, right, numComparisons):
         pivotIndex = left + medianIndex
     else:
         pivotIndex = right
-    '''
+    #'''
+
+    pivotIndex = random.randint(left, right)
+    partitionIndex = partition(arr, pivotIndex, left, right)
+    quickSort(arr, left, partitionIndex - 1, numComparisons)
+    quickSort(arr, partitionIndex + 1, right, numComparisons)
+
+    return numComparisons
+
+'''
+partition subroutine
+input: an array of integers, index of chosen pivot, leftmost index, rightmost index
+output: index of pivot after array has been partitioned around the pivot value
+'''
+def partition(arr, pivotIndex, left, right):
     if pivotIndex != left:
         arr[left], arr[pivotIndex] = arr[pivotIndex], arr[left]
     #print 'pivot: ' + str(arr[left]) + ' pivotIndex: ' + str(pivotIndex) + ' new array: ' + str(arr)
@@ -42,31 +53,27 @@ def quickSort(arr, left, right, numComparisons):
     arr[left], arr[partitionIndex] = arr[partitionIndex], arr[left]
     #print 'partitionIndex: ' + str(partitionIndex) + ' updated array: ' + str(arr)
 
-    #print 'SORT LEFT...',
-    #numComparisons[0] += partitionIndex - left - 1
-    quickSort(arr, left, partitionIndex - 1, numComparisons)
-    #print 'SORT RIGHT...',
-    #numComparisons[0] += right - partitionIndex - 1
-    quickSort(arr, partitionIndex + 1, right, numComparisons)
+    return partitionIndex
 
-    return
 '''
 # Algorithms Week 3 Assignment
-# Attempt 1:
-#   a)
-#
+# CORRECT:
+#   a) 162085
+#   b) 164123
+#   c) 138382
 '''
 '''
+import sys
 with open('TestData-QuickSort.txt') as f:
     data = f.read()
 
 strArray = data.strip().split('\n')
 intArray = [int(numStr) for numStr in strArray]
 
-count = [0]
-quickSort(intArray,0,9999, count)
+#count = [0]
+print(quickSort(intArray,0,9999))
 #print(intArray)
-print(count)
+#print(count)
 #'''
 
 ''' TEST CASES '''
